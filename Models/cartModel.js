133e802a2d7ce31e25fce4c49cedc6edf5ac7060
ahamedroshan8883.h <ahamed8883.h@gmail.com>
2024-10-10 21:50:32 +0530

    quantity misake corrected

--- Models/cartModel.js
+++ Models/cartModel.js
@@ -6,7 +6,7 @@ const cartSchema = new mongoose.Schema({
         title: { type: String, required: true },// Product name
         image: {type:String,required:true},//Product image url
         price: { type: Number, required: true }, // Product price
-        quantity: { type: Number, default:1 }, // Fixed typo from "Quatity"
+        quantity: { type: Number }, // Fixed typo from "Quatity"
         selectedSize: { type: String, default:'S'} // Fixed typo from "SelectedSize" and used camelCase
     }],
     totalQuantity: { type: Number, default: 0 }, // Total quantity of products in the cart
